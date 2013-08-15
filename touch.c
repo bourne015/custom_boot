d909de1323ffdef6af0bca249410c62520f0fc57
tao <tao@tao-hp.(none)>
2013-08-15 23:58:39 +0800

    paint when touch the screen

--- touch.c
+++ touch.c
@@ -2,6 +2,41 @@
 
 void puts(char *);
 
+void stylus_up_down(void)
+{
+	if ((ADCDAT0 & (1<<15))) {
+		//stylus up
+		full_color(0x00ddff);
+		//detect stylus down
+		ADCTSC = 0xd3;
+	} else {
+		//stylus down
+		//wait up
+		//ADCTSC = 0x1d3;
+		
+		//set to auto mode,disable pull-up
+		ADCTSC = (1<<2) | (1<<3);
+		full_color(0xffdd00);
+
+		//start adc convert
+		ADCCON |= 0x01;
+	}
+
+	ADCCLRINTPNDNUP = 0x0;
+}
+
+void stylus_adc(void)
+{
+	volatile unsigned long x, y;
+
+	x = ADCDAT0 & 0x3ff;
+	y = ADCDAT1 & 0x3ff;
+	//wait stylus up
+	ADCTSC = 0x1d3;
+
+	ADCCLRINT = 0x0;
+}
+
 void touch_init(void)
 {
 	/*pclk:66Mhz, AD freq:2Mhz*/
@@ -9,41 +44,17 @@ void touch_init(void)
 
 	ADCDLY = 10000 & 0xffff;
 	
+	//enable updn and adc irq,and irq_handler
+	VIC1VECTADDR30 = stylus_up_down;
+	VIC1VECTADDR31 = stylus_adc;
+	VIC1INTENABLE |= (1<<31) | (1<<30);
+
 	//ADCTSC = (0<<8) | (1<<7) | (1<<6) | (1<<4) | (0x3);
 	ADCTSC = 0xd3;
-	
-}
-
-void int_pen_up_down(void)
-{
-	unsigned char val;
-	
-	val = ADCUPDN;
-	if (val & 0x01) {
-		puts("down\n\r");
-		ADCCON |= 0x01;
-	} else if (val &0x02) {
-		puts("up\n\r");
-		ADCCON |= 0x01;
-	}
-
-	ADCCLRINTPNDNUP = 0x0;
-}
-
-void int_adc(void)
-{
-	unsigned long val;
-
-	val = ADCDAT0;
-
-	ADCCLRINT = 0x0;
 }
 
 void touch_test(void)
 {
 	touch_init();
-
-	VIC1VECTADDR30 = int_pen_up_down;
-	VIC1VECTADDR31 = int_adc;
-	VIC1INTENABLE |= (1<<31) | (1<<30);
+	key_irq_init();	
 }
