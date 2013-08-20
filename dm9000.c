06db9690ff8ccffb1c58c9f8b03a4864e4a92a46
tao <phantasy015@gmail.com>
2013-08-20 20:07:17 +0800

    send function, need test

--- dm9000.c
+++ dm9000.c
@@ -2,17 +2,18 @@
 #include "dm9000.h"
 
 #define dm9000_outb(val, addr)	(*(unsigned char *)addr = val)
-#define dm9000_inb(addr)		(*(unsigned char *)addr)
+#define dm9000_inb(addr)	(*(unsigned char *)addr)
+#define dm9000_outw(val, addr)	(*(u16 *)addr = val)
 
 void dm9000_iow(int reg, unsigned char val)
 {
-	dm9000_oub(reg, DM9000_IO);
-	dm9000_oub(val, DM9000_DATA);
+	dm9000_outb(reg, DM9000_IO);
+	dm9000_outb(val, DM9000_DATA);
 }
 
 void dm9000_ior(int reg)
 {
-	dm9000_oub(reg, DM9000_IO);
+	dm9000_outb(reg, DM9000_IO);
 	
 	return (dm9000_inb(DM9000_DATA));
 }
@@ -38,43 +39,68 @@ void dm9000_reset(void)
 void dm9000_probe(void)
 {
 	unsigned long in_val;
+	int i;
+	
+	/*try multiple times, codes from kernel*/
+	for (i = 0; i < 8; i++) {
+		id_val  = dm9000_ior(VIDL);
+		id_val |= dm9000_ior(VIDH) << 8;
+		id_val |= dm9000_ior(PIDL) << 16;
+		id_val |= dm9000_ior(PIDH) << 24;
 
-	id_val  = dm9000_ior(VIDL);
-	id_val |= dm9000_ior(VIDH) << 8;
-	id_val |= dm9000_ior(PIDL) << 16;
-	id_val |= dm9000_ior(PIDH) << 24;
-
+		if (id_val == DM9000_ID)
+			break;
+		printf("read wrong id %lu, ty again\n\r", id_val);
+	}
+	
 	if (id_val != DM9000_ID) {
-		puts("dm9000 not found\n\r");
+		printf("wrong id %lu\n\r", id_val);
 		return -1;
 	}
 
-	puts("found dm9000\n\r");
 	return 0;
 }
 
-static int dm9000_send(void *packet, int length)
+static int dm9000_send(volatile void *packet, int length)
 {
+	int i = 0, count;
+
 	/*clear tx bit*/
 	dm9000_iow(ISR, (0x1<<1));
 	
-	/*move data to tx ram*/
+	/*prapare to move data to tx ram*/
 	dm9000_outb(MWCMD, DM9000_IO);
-
+	
+	/*push data to the tx-fifo*/
+	count = (length + 1)>>2;
+	for (i = 0; i < count; i++)
+		dm9000_outw((u16 *)packet[i], DM9000_DATA);
+	
+	dm9000_iow(TCR, 0x1);
+	/*set tx length*/
 	dm9000_iow(TXPLL, length & 0xff);
 	dm9000_iow(TXPLH, (length>>8) & 0xff)
-	dm9000_iow(ISR, (0x1<<1));
 	
+	/*wait for end of transmission*/
+	while (!(dm9000_ior(NSR) & ((1<<3) | (1<<2))));
+
+	dm9000_iow(ISR, (0x1<<1));
+
 	return 0;
 }
 
-static int dm9000_rx(void)
+static int dm9000_rx(volatile void *packet)
 {
+	int length = 0;
+
+	
 	return 0;
 }
 
 static int dm9000_init(void)
 {
+	int i = 0;
+
 	dm9000_reset();
 	
 	if (dm9000_probe() < 0) {
@@ -82,9 +108,20 @@ static int dm9000_init(void)
 	}
 
 	dm9000_iow(NCR, 0x0);
+	/*clear tx status*/
 	dm9000_iow(NSR, ((1<<5) | (1<<3) | (1<<2)));
+	/*clear interrupt status*/
 	dm9000_iow(ISR, ((1<<3) | (1<<2) | (1<<1) | (1)));
-
+	/*
+	* enable sram point return to start, when
+	* the point address are over the sram size
+	*/
 	dm9000_iow(IMR, (1<<7));
+	/*rx enable*/
 	dm9000_iow(RCR, ((1<<5) | (1<<4) (1<<0)))
+
+	/*read mac address*/
+	for (i = 0; i < 6; i++) {
+		printf("mac addr: %c ", dm9000_ior(PAR+i));
+	}
 }
--- dm9000.h
+++ dm9000.h
@@ -1,11 +1,13 @@
 #define DM9000_IO	0x18000000
 #define DM9000_DATA	0x18000004
+#define DM9000_ID	0x90000a46
 
 #define NCR	0x00
 #define NSR	0x01
 #define TCR	0x02
 #define RCR	0x05
 #define RSR	0x06
+#define PAR	0x10
 #define GPR	0x1f
 #define TXPLL	0xFC
 #define TXPLH	0xFd
